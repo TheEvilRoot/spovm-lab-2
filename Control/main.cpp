3c01a66a2d835098bd600690f789a0abe37c1a73
TheEvilRoot <robot-creatix@ya.ru>
2020-04-01 12:34:10 +0400

    Intial

---
+++ Control/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cwchar>
+#include <vector>
+
+#include <Windows.h>
+#include <conio.h>
+
+typedef PROCESS_INFORMATION ProcessInfo;
+typedef STARTUPINFOA StartUpInfo;
+
+ProcessInfo * createHandler() {
+  StartUpInfo startUpInfo;
+  ZeroMemory(&startUpInfo, sizeof(startUpInfo));
+  startUpInfo.cb = sizeof(startUpInfo);
+
+  ProcessInfo *processInfo = new ProcessInfo;
+  ZeroMemory(processInfo, sizeof(processInfo));
+
+  CreateProcessA(nullptr, LPSTR("Handler.exe"), nullptr, nullptr, false,
+                 0, nullptr, nullptr, &startUpInfo,
+                 processInfo);
+
+  return processInfo;
+}
+
+DWORD getProcessExitCode(const ProcessInfo * info) {
+  DWORD exitCode;
+  GetExitCodeProcess(info->hProcess, &exitCode);
+  return exitCode;
+}
+
+void newProcess(std::vector<ProcessInfo *> &processes) {
+  processes.push_back(createHandler());
+}
+
+void deleteProcess(std::vector<ProcessInfo *> &processes) {
+  if (!processes.empty()) {
+    auto process = processes.back();
+    processes.pop_back();
+    TerminateProcess(process->hProcess, 0);
+    WaitForSingleObject(process->hProcess, INFINITE);
+  }
+}
+
+void terminateProcesses(std::vector<ProcessInfo *> &processes) {
+  for (auto process : processes) {
+    TerminateProcess(process->hProcess, 0);
+    WaitForSingleObject(process->hProcess, INFINITE);
+  }
+}
+
+bool handleInput(std::vector<ProcessInfo *> &processes) {
+  char c = _getch();
+  switch (c) {
+  case '+':
+    newProcess(processes);
+    break;
+  case '-':
+    deleteProcess(processes);
+    break;
+  case 'q':
+    terminateProcesses(processes);
+    return false;
+  }
+  return true;
+}
+
+void handleSelfTerminated(std::vector<ProcessInfo *> &processes) {
+  std::vector<ProcessInfo *> allocator;
+  for (const auto process : processes) {
+    if (getProcessExitCode(process) != STILL_ACTIVE) {  
+      delete process;
+    } else {
+      allocator.push_back(process);
+    }
+  }
+  processes = allocator;
+}
+
+int main(const int argc, const char *argv[]) { 
+  auto allowedEvent = CreateEventA(nullptr, false, true, "Allowed");
+  std::vector<ProcessInfo *> processes;
+
+  while (true) {
+    if (!handleInput(processes))
+      break;
+
+    handleSelfTerminated(processes);
+  }
+
+  
+  return 0;
+}
---
+++ Handler/handler.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+
+#include <Windows.h>
+
+int getRandomInt(int min, int max) {
+  return min + (rand() % static_cast<int>(max - min + 1));
+}
+
+char *getRandomString(size_t size) {
+  char *string = new char[size + 1];
+
+  for (size_t i = 0; i < size; i++) {
+    if (getRandomInt(0, 3) > 1) {
+      string[i] = getRandomInt('A', 'Z');
+    } else {
+      string[i] = getRandomInt('a', 'z');
+    }
+  }
+
+  string[size] = '\0';
+  return string;
+}
+
+void waitForQueue(HANDLE &allowedEvent) { 
+  WaitForSingleObject(allowedEvent, INFINITE);
+}
+
+int main(const int argc, const char *argv[]) {
+  srand(time(NULL));
+  
+  auto allowedEvent = OpenEventA(EVENT_ALL_ACCESS, false, "Allowed");
+  char *string = getRandomString(10);
+
+  while (true) {
+    waitForQueue(allowedEvent);
+
+    for (int i = 0; string[i]; i++) {
+      Sleep(100);
+      fprintf(stderr, "%c", string[i]);
+    }
+    fprintf(stderr, "\n");
+
+    SetEvent(allowedEvent);
+  }
+  
+  return 0;
+}
